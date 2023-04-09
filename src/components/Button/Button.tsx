f387c3a5500e9e05b02f29a67e9f95a31ec387a9
kanat.berkinbayev <Kanat.Berkinbayev@nitec.kz>
2023-04-09 17:34:09 +0600

    react

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 
 const Button = (props: any) => {
+    let className = "btn-default";
+    if (props.type === 'primary') {
+        className = "btn-primary";
+    }
+    if (props.type === 'default') {
+        className = "btn-default";
+    }
+    if (props.type === 'link') {
+        className = "btn-link";
+    }
+    if (props.type === 'secondary') {
+        className = "btn-secondary";
+    }
+
+
     return (
         <>
-
+            <button type="button" className={`btn ${className} ${props.className}`} onClick={props.onClick} disabled={props.disabled}>{props.label}</button>
         </>
     )
 }
