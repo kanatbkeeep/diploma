83caa57b91ef024f4d0d42ee61fe67310a704f3b
kanatbkeeep <clashofkanat@gmail.com>
2023-04-12 09:46:12 +0600

    Buttons final

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import './App.css';
 import './style/common.scss';
-import { store } from './store/store'
-import { Provider } from 'react-redux'
+import {store} from './store/store'
+import {Provider} from 'react-redux'
 import Button from './components/Button/Button'
+
 function App() {
-  return (
-      <Provider store={store}>
-        <Button
-            label={'Button'}
-        />
-      </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <Button
+                type={'primaryButton'}
+                label={'Button'}
+            />
+            <br/>
+            <br/>
+            <Button
+                type={'secondaryButton'}
+                label={'Button'}
+            />
+            <br/>
+            <br/>F
+            <Button
+                type={'primaryButtonAdd'}
+                label={'Button'}
+            />
+            <br/>
+            <Button
+                type={'secondaryButtonAdd'}
+                label={'Button'}
+            />
+        </Provider>
+    );
 }
 
 export default App;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import Plus from '../../assets/icon/plus.svg'
 
 const Button = (props: any) => {
+    //типы которые возможны primaryButton, secondaryButton
+    //типы с плюсом primaryButtonAdd, secondaryButtonAdd
     let classname: string = props.type;
     return (
         <>
-            <button type="button" className={`${classname? classname : 'primaryButton'} ${props.className}`} onClick={props.onClick} disabled={props.disabled}>{props.label}</button>
+            <button type="button" className={`${classname ? classname : 'primaryButton'} ${props.className}`}
+                    onClick={props.onClick}
+                    disabled={props.disabled}>{classname === 'primaryButtonAdd' || classname === 'secondaryButtonAdd' ? <>
+                <img src={Plus} alt={'icon'}/> {props.label}</> : props.label}
+            </button>
         </>
     )
 }
