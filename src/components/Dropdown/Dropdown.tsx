dc447bc98bf0075df78ff750a8c99706c5d5a76e
Daniyar.Myrzassary <Daniyar.Myrzassary@nitec.kz>
2023-04-18 22:46:51 +0600

    fix table

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,18 @@
+import React,{useState} from "react";
+
+const Dropdown = (props:any) =>{
+
+    return(
+        <div className="dropdown">
+            <div className="dropdown-btn">Выбирай</div>
+            <div className="dropdown-content">
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+
+export default Dropdown;
