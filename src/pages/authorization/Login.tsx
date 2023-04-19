9bdc48cb2adb3cb2a39653ac78a182085c790b68
kanatbkeeep <clashofkanat@gmail.com>
2023-04-19 12:04:24 +0600

    login page

--- src/pages/authorization/Login.tsx
+++ src/pages/authorization/Login.tsx
@@ -2,22 +2,23 @@ import React from 'react';
 import {store} from '../../store/store'
 import Logo from '../../assets/icon/logo.svg'
 import '../../style/loginPage.scss';
+import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
 
 function Login() {
     return (
         <>
             <main className="bg_darker_blue full_screen centralized">
                 <section className="bg_white loginBlock">
-                    <img src={Logo}/>
+                    <img className={'logo'} src={Logo} alt={'logo'}/>
 
-                    <section>
-                        <h2></h2>
-                        <iframe frameBorder="0" width="100%" height="180"
-                                src="https://music.yandex.ru/iframe/#track/111161321/24799113">Слушайте <a
-                            href='https://music.yandex.ru/album/24799113/track/111161321'>Triple 6</a> — <a
-                            href='https://music.yandex.ru/artist/13133796'>TopConniver</a> на Яндекс Музыке
-                        </iframe>
-                    </section>
+                    <aside>
+                        <h2>{'Log in'}</h2>
+                        <Input label={'Email'}/>
+                        <Input label={'Password'} type={'password'}/>
+                        <Button label={'Log in'}/>
+                        <a>{'Forgot password?'}</a>
+                    </aside>
                 </section>
             </main>
         </>
