30c93fc0e0629973d0931f91427ebda8bbc3463d
Daniyar.Myrzassary <Daniyar.Myrzassary@nitec.kz>
2023-04-19 23:03:09 +0600

    table

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import {store} from './store/store'
 import {Provider} from 'react-redux'
 import Button from './components/Button/Button'
 import Table from "./components/Table/Table";
+import Login from "./pages/authorization/Login";
 import Input from "./components/Input/Input";
+import Checkbox from "./components/Checkbox/Checkbox";
 
 function App() {
     const arr = Array.from({ length: 40 }, (_, i) => ({
@@ -15,60 +17,13 @@ function App() {
         middlename: "Timurylu",
         age: 20,
     }));
-
-
     return (
-        <Provider store={store}>
+        // <Login/>
 
-            <Input
-                label="Email"
-            maxWidth={200}
-            />
-
-            <Input
-                type="password"
-                label="Passowrd"
-                maxWidth={200}
-            />
-
-            <Button
-                type={'primaryButton'}
-                label={'Button'}
-            />
-            <br/>
-            <br/>
-            <Button
-                type={'secondaryButton'}
-                label={'Button'}
-            />
-            <br/>
-            <br/>
-            <Button
-                type={'primaryButtonAdd'}
-                label={'Button'}
-            />
-            <br/>
-            <Button
-                type={'secondaryButtonAdd'}
-                label={'Button'}
-            />
-            <br/>
-            <Button
-                type={'smallBlue'}
-            />
-            <br/>
-            <br/>
-            <Button
-                type={'smallRed'}
-            />
-            <br/>
-            <br/>
-            <Button
-                type={'smallDark'}
-            />
-            <br/>
-            <br/>
+        <>
+            <div style={{marginBottom: 16}}/>
             <Table
+                search={true}
                 array={arr}
                 rowsPerPage={4}
                 maxWidthTable={800}
@@ -98,17 +53,13 @@ function App() {
                     );
                 }}
             />
-
-            <br/>
-
-            <div style={{backgroundColor:"black"}}>
-                <svg width="14" height="11" viewBox="0 0 14 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M5.5537 7.55928L12.005 0.804521C12.3928 0.398493 13.0216 0.398493 13.4094 0.804521C13.7971 1.21055 13.7971 1.86885 13.4094 2.27488L5.5537 10.5L0.991038 5.72275C0.603248 5.31672 0.603248 4.65842 0.991038 4.25239C1.37883 3.84636 2.00756 3.84636 2.39535 4.25239L5.5537 7.55928Z" fill="white"/>
-                </svg>
-            </div>
-
-        </Provider>
-    );
+            <div style={{marginBottom: 16}}/>
+            <Checkbox
+                id="name"
+            label="Daniyar"
+            />
+        </>
+    )
 }
 
 export default App;
