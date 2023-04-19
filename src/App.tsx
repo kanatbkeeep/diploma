ef83dfd311c7c15f27b7978126b56f16ccdb1516
Daniyar.Myrzassary <Daniyar.Myrzassary@nitec.kz>
2023-04-19 23:03:09 +0600

    table

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,58 @@ import Button from './components/Button/Button'
 import Table from "./components/Table/Table";
 import Login from "./pages/authorization/Login";
 import Input from "./components/Input/Input";
+import Checkbox from "./components/Checkbox/Checkbox";
 
 function App() {
+    const arr = Array.from({ length: 40 }, (_, i) => ({
+        id:i,
+        firstname: `Firstname${i}`,
+        lastname: "Myrzasary",
+        middlename: "Timurylu",
+        age: 20,
+    }));
     return (
-        <Login/>
+        // <Login/>
+
+        <>
+            <div style={{marginBottom: 16}}/>
+            <Table
+                search={true}
+                array={arr}
+                rowsPerPage={4}
+                maxWidthTable={800}
+                maxWidthColumns={[200, 200, 200, 200]}
+                haveDelete={true}
+                onDelete={()=>{
+                    console.log("deleted");
+                }}
+                renderHead={(maxWidthColumns) => {
+                    return <div>
+                        <div style={{maxWidth:50}}></div>
+                        <div style={{maxWidth: maxWidthColumns[0]}}>Имя</div>
+                        <div style={{maxWidth: maxWidthColumns[1]}}>Фамилия</div>
+                        <div style={{maxWidth: maxWidthColumns[2]}}>Отчество</div>
+                        <div style={{maxWidth: maxWidthColumns[3]}}>Возраст</div>
+                    </div>
+                }}
+                renderBody={(item, index, maxWidthColumns, checkbox) => {
+                    return (
+                        <div key={index}>
+                            <div style={checkbox ? {maxWidth:50} : {}}>{checkbox}</div>
+                            <div style={{maxWidth: maxWidthColumns[0]}}>{item.firstname}</div>
+                            <div style={{maxWidth: maxWidthColumns[1]}}>{item.lastname}</div>
+                            <div style={{maxWidth: maxWidthColumns[2]}}>{item.middlename}</div>
+                            <div style={{maxWidth: maxWidthColumns[3]}}>{item.age}</div>
+                        </div>
+                    );
+                }}
+            />
+            <div style={{marginBottom: 16}}/>
+            <Checkbox
+                id="name"
+            label="Daniyar"
+            />
+        </>
     )
 }
 
--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -2,17 +2,28 @@ import React from 'react';
 
 const Checkbox = (props:any) =>{
     return(
-        <div className="checkbox-group">
-            <input
-                type="checkbox"
-                onChange={props.onChange}
-                id={`checkbox${props.id ? props.id : ""}`}
-                defaultChecked={props.defaultChecked}
-                checked={props.checked}
-                disabled={props.disabled}
+        <label className="checkbox-group">
+            <input type="checkbox"
+                   onChange={props.onChange}
+                   id={`checkbox${props.id ? props.id : ""}`}
+                   defaultChecked={props.defaultChecked}
+                   checked={props.checked}
+                   disabled={props.disabled}
             />
-            <label htmlFor={`checkbox${props.label}`}>{props.label}</label>
-        </div>
+                <span className="checkmark"></span>
+                {props.label}
+        </label>
+        // <div className="checkbox-group">
+        //     <input
+        //         type="checkbox"
+        //         onChange={props.onChange}
+        //         id={`checkbox${props.id ? props.id : ""}`}
+        //         defaultChecked={props.defaultChecked}
+        //         checked={props.checked}
+        //         disabled={props.disabled}
+        //     />
+        //     <label htmlFor={`checkbox${props.label}`}>{props.label}</label>
+        // </div>
     )
 }
 
