ba63f11caea95e3c0ee8230056c604d1c2409bdb
Daniyar.Myrzassary <Daniyar.Myrzassary@nitec.kz>
2023-04-20 23:33:46 +0600

    table

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,21 @@ import Input from "./components/Input/Input";
 import Checkbox from "./components/Checkbox/Checkbox";
 
 function App() {
-    const arr = Array.from({ length: 40 }, (_, i) => ({
-        id:i,
-        firstname: `Firstname${i}`,
-        lastname: "Myrzasary",
-        middlename: "Timurylu",
-        age: 20,
-    }));
+    // const arr = Array.from({ length: 40 }, (_, i) => ({
+    //     id:i,
+    //     firstname: i,
+    //     lastname: "Myrzasary"+i,
+    //     middlename: "Timurylu",
+    //     age: 20,
+    // }));
+
+    const arr = [
+        { id: 1, firstname: "Alice", lastname: "Smith", middlename: "Marie", age: 25 },
+        { id: 2, firstname: "Bob", lastname: "Johnson", middlename: "William", age: 30 },
+        { id: 3, firstname: "Charlie", lastname: "Brown", middlename: "Thomas", age: 40 },
+        { id: 4, firstname: "David", lastname: "Lee", middlename: "Joseph", age: 20 },
+        { id: 5, firstname: "Emma", lastname: "Lee", middlename: "Sophia", age: 35 },
+    ];
     return (
         // <Login/>
 
