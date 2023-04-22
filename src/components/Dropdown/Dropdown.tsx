f0017ec597f95693ab32926108fceb2da972b8dc
Daniyar.Myrzassary <Daniyar.Myrzassary@nitec.kz>
2023-04-22 21:32:17 +0600

    dropdown

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,15 +1,19 @@
-import React,{useState} from "react";
+import React, {useState} from "react";
+import arrow from '../../assets/icon/arrow.svg'
 
-const Dropdown = (props:any) =>{
+const Dropdown = (props: any) => {
+    const [isActive, setIsActive] = useState(false)
 
-    return(
-        <div className="dropdown">
-            <div className="dropdown-btn">Выбирай</div>
-            <div className="dropdown-content">
-                <ul>
-                    <li></li>
-                </ul>
+
+    return (
+        <div className="dropdown" style={{maxWidth: props.maxWidth}} onClick={props.onClick && props.onClick()}>
+            <div className={isActive ? "dropdown-btn dropdown-btn-active" : "dropdown-btn"} onClick={()=>{setIsActive(!isActive)}}>
+                {props.value}
+                <img src={arrow} style={{transform: isActive ? "rotate(180deg)" : ""}}/>
             </div>
+            {isActive && (
+                props.children
+            )}
         </div>
     )
 }
