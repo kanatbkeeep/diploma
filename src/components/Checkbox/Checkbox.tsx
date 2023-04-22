38b8c518d9a1d2e4310ba8964eb953660899d318
Daniyar.Myrzassary <Daniyar.Myrzassary@nitec.kz>
2023-04-22 22:17:29 +0600

    dropdown

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Checkbox = (props:any) =>{
+
     return(
         <label className="checkbox-group">
             <input type="checkbox"
