2a0132004ad9bada927b91b553c179d2c959fabd
Daniyar.Myrzassary <Daniyar.Myrzassary@nitec.kz>
2023-04-23 00:03:24 +0600

    add files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import './style/common.scss';
-import {store} from './store/store'
 import {Provider} from 'react-redux'
 import Button from './components/Button/Button'
 import Table from "./components/Table/Table";
@@ -9,6 +8,7 @@ import Dropdown from "./components/Dropdown/Dropdown";
 import Login from "./pages/authorization/Login";
 import Input from "./components/Input/Input";
 import Checkbox from "./components/Checkbox/Checkbox";
+import Profile from "./pages/profile/Profile";
 
 
 function App() {
@@ -28,102 +28,8 @@ function App() {
         { id: 5, firstname: "Emma", lastname: "Lee", middlename: "Sophia", age: 35 },
     ];
     return (
-
-        // <Login/>
-
         <>
-            <div style={{marginBottom: 16}}/>
-
-           <>
-            <Dropdown
-            maxWidth={300}
-            value='Select'
-            >
-                <ul>
-                    <li>React</li>
-                    <li>React</li>
-                    <li>React</li><li>React</li>
-                    <li>React</li>
-                    <li>React</li>
-                    <li>React</li>
-                </ul>
-            </Dropdown>
-            <Button
-                type={'primaryButton'}
-                label={'Button'}
-            />
-            <br/>
-            <br/>
-            <Button
-                type={'secondaryButton'}
-                label={'Button'}
-            />
-            <br/>
-            <br/>
-            <Button
-                type={'primaryButtonAdd'}
-                label={'Button'}
-            />
-            <br/>
-            <Button
-                type={'secondaryButtonAdd'}
-                label={'Button'}
-            />
-            <br/>
-            <Button
-                type={'smallBlue'}
-            />
-            <br/>
-            <br/>
-            <Button
-                type={'smallRed'}
-            />
-            <br/>
-            <br/>
-            <Button
-                type={'smallDark'}
-            />
-            <br/>
-            <br/>
-            <div style={{marginBottom: 16}}/>
-
-
-            <Table
-                search={true}
-                array={arr}
-                rowsPerPage={4}
-                maxWidthTable={800}
-                maxWidthColumns={[200, 200, 200, 200]}
-                haveDelete={true}
-                onDelete={()=>{
-                    console.log("deleted");
-                }}
-                renderHead={(maxWidthColumns) => {
-                    return <div>
-                        <div style={{maxWidth:50}}></div>
-                        <div style={{maxWidth: maxWidthColumns[0]}}>Имя</div>
-                        <div style={{maxWidth: maxWidthColumns[1]}}>Фамилия</div>
-                        <div style={{maxWidth: maxWidthColumns[2]}}>Отчество</div>
-                        <div style={{maxWidth: maxWidthColumns[3]}}>Возраст</div>
-                    </div>
-                }}
-                renderBody={(item, index, maxWidthColumns, checkbox) => {
-                    return (
-                        <div key={index}>
-                            <div style={checkbox ? {maxWidth:50} : {}}>{checkbox}</div>
-                            <div style={{maxWidth: maxWidthColumns[0]}}>{item.firstname}</div>
-                            <div style={{maxWidth: maxWidthColumns[1]}}>{item.lastname}</div>
-                            <div style={{maxWidth: maxWidthColumns[2]}}>{item.middlename}</div>
-                            <div style={{maxWidth: maxWidthColumns[3]}}>{item.age}</div>
-                        </div>
-                    );
-                }}
-            />
-            <div style={{marginBottom: 16}}/>
-            <Checkbox
-                id="name"
-            label="Daniyar"
-            />
+           <Profile/>
         </>
     )
 }
