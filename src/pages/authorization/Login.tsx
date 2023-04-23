7428c452ce84448bc724c734336fb9f7ab43a917
kanat.berkinbayev <Kanat.Berkinbayev@nitec.kz>
2023-04-23 15:56:45 +0600

    login

--- src/pages/authorization/Login.tsx
+++ src/pages/authorization/Login.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Logo from '../../assets/icon/logo.svg'
 import '../../style/loginPage.scss';
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
+import CurrentUserStore from "../../store/CurrentUserStore";
 
 function Login() {
+    useEffect(() => {
+
+    }, [])
+
     return (
         <>
             <main className="bg_darker_blue full_screen centralized">
@@ -13,9 +18,16 @@ function Login() {
 
                     <aside>
                         <h2>{'Log in'}</h2>
-                        <Input label={'Email'}/>
-                        <Input label={'Password'} type={'password'}/>
-                        <Button label={'Log in'}/>
+                        <Input label={'Email'} onChange={(e: any) => {
+                            CurrentUserStore.editModel({email: e.target.value});
+                            console.log(e.target.value)
+                        }}/>
+                        <Input label={'Password'} type={'password'} onChange={(e: any) => {
+                            CurrentUserStore.editModel({password: e.target.value})
+                        }}/>
+                        <Button label={'Log in'} onClick={() => {
+                            CurrentUserStore.loadLogin()
+                        }}/>
                         <a>{'Forgot password?'}</a>
                     </aside>
                 </section>
--- src/store/CurrentUserStore.ts
+++ src/store/CurrentUserStore.ts
@@ -1,19 +1,53 @@
 import {action, makeAutoObservable, runInAction} from 'mobx';
 import React from 'react';
+import axios from 'axios'
 
 class CurrentUserStore {
     currentUser: any;
     model: any;
+
+    loadLogin() {
+        axios.post('http://localhost:8080/user/loginUser', {
+            email: this.model.email,
+            password: this.model.password
+        }).then((repos) => {
+            document.cookie = "Authorization=" + repos.data;
+            this.getUser();
+        });
+
+    }
+
+    getUser() {
+        axios.get('http://localhost:8080/user/getUser', {headers: {
+                Authorization: this.getCookie('Authorization')
+            }}).then((repos) => {
+            this.currentUser = repos.data;
+            window.location.replace('/');
+        });
+    }
+
+    getCookie(name: any) {
+        const value = `; ${document.cookie}`;
+        const parts: any = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+    }
 
     editModel(obj: any) {
         this.model = {...this.model, ...obj};
     };
 
     constructor() {
-        this.model = {}
+        this.model = {
+            email: null,
+            password: null,
+        }
+
         makeAutoObservable(this, {
             editModel: action.bound,
+            loadLogin: action.bound,
+            getUser: action.bound,
         })
     }
-
 }
+
+export default new CurrentUserStore() as CurrentUserStore;
