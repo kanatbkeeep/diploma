360794c844c3dd83602db0a554df919ab4cb0618
Daniyar.Myrzassary <Daniyar.Myrzassary@nitec.kz>
2023-04-23 18:53:57 +0600

    add files

--- src/pages/creation_plan/CreationPlan.tsx
+++ src/pages/creation_plan/CreationPlan.tsx
@@ -10,6 +10,8 @@ import Download from '../../assets/icon/download.svg';
 import Logout from '../../assets/icon/logout.svg';
 import Navigation from "../../components/Notification/Notification";
 import Step1 from "../../components/CreationPlan/steps/Step1";
+import Step2 from "../../components/CreationPlan/steps/Step2";
+import Step3 from "../../components/CreationPlan/steps/Step3";
 
 
 export enum Steps {
@@ -69,27 +71,27 @@ const CreationPlan = (props: any) => {
 
             <section>
                 <div className="nav-steps mt-42">
-                    <div className="nav-step">
+                    <div className="nav-step" onClick={()=>{setStep(Steps.Step1)}}>
                         <div style={step === Steps.Step1 ? {boxShadow:"0px 5px 6px -3px #007EA7",color:"#007EA7"} : {}}>1</div>
                          <p style={step === Steps.Step1 ? {color:"#007EA7"} : {}}>Academic work</p>
                     </div>
-                    <div className="nav-step">
+                    <div className="nav-step" onClick={()=>{setStep(Steps.Step2)}}>
                         <div style={step === Steps.Step2 ? {boxShadow:"0px 5px 6px -3px #007EA7",color:"#007EA7"} : {}}>2</div>
                         <p style={step === Steps.Step2 ? {color:"#007EA7"} : {}}>Educational Methodological work</p>
                     </div>
-                    <div className="nav-step">
+                    <div className="nav-step" onClick={()=>{setStep(Steps.Step3)}}>
                         <div style={step === Steps.Step3 ? {boxShadow:"0px 5px 6px -3px #007EA7",color:"#007EA7"} : {}}>3</div>
                         <p style={step === Steps.Step3 ? {color:"#007EA7"} : {}}>Research work</p>
                     </div>
-                    <div className="nav-step">
+                    <div className="nav-step" onClick={()=>{setStep(Steps.Step4)}}>
                         <div style={step === Steps.Step4 ? {boxShadow:"0px 5px 6px -3px #007EA7",color:"#007EA7"} : {}}>4</div>
                         <p style={step === Steps.Step4 ? {color:"#007EA7"} : {}}>Educational work</p>
                     </div>
-                    <div className="nav-step">
+                    <div className="nav-step" onClick={()=>{setStep(Steps.Step5)}}>
                         <div style={step === Steps.Step5 ? {boxShadow:"0px 5px 6px -3px #007EA7",color:"#007EA7"} : {}}>5</div>
                         <p style={step === Steps.Step5 ? {color:"#007EA7"} : {}}>Social work</p>
                     </div>
-                    <div className="nav-step">
+                    <div className="nav-step" onClick={()=>{setStep(Steps.Step6)}}>
                         <div style={step === Steps.Step6 ? {boxShadow:"0px 5px 6px -3px #007EA7",color:"#007EA7"} : {}}>6</div>
                         <p style={step === Steps.Step6 ? {color:"#007EA7"} : {}}>KPI</p>
                     </div>
@@ -98,6 +100,12 @@ const CreationPlan = (props: any) => {
                 <div className="step">
                     {
                         step === Steps.Step1 ? <Step1/> : null
+                    }
+                    {
+                        step === Steps.Step2 ? <Step2/> : null
+                    }
+                    {
+                        step === Steps.Step3 ? <Step3/> : null
                     }
                 </div>
             </section>
