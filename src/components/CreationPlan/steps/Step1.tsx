0bd21c47bc4553ca52d6c303f55af7715927e5b1
Daniyar.Myrzassary <Daniyar.Myrzassary@nitec.kz>
2023-04-23 18:53:57 +0600

    add files

--- src/components/CreationPlan/steps/Step1.tsx
+++ src/components/CreationPlan/steps/Step1.tsx
@@ -35,7 +35,7 @@ const Step1 = () =>{
     return(
         <div className="step-component">
             <div className="years">2022-2023</div>
-            <div className="inputs-step-1">
+            <div className="inputs-step">
                 <div style={{marginBottom:20, display:"flex"}}>
                     <Input
                      maxWidth={500}
