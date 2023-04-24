488c38f1bafb95c9df179079429e1bfcc0ce2dad
kanatbkeeep <clashofkanat@gmail.com>
2023-04-24 11:53:39 +0600

    profile

--- src/utils/lang/en.ts
+++ src/utils/lang/en.ts
@@ -2,6 +2,18 @@ const en = {
     loginTitle: 'Log in',
     password: 'Password',
     forgotPassword: 'Forgot password?',
+    academicYear: 'Academic year',
+    academicWork: 'Academic work',
+    academicMethods: 'Ed. & Method. work',
+    academicResearchWork: 'Research work',
+    academicEducationalWork: 'Educational work',
+    academicSocialWork: 'Social work',
+    status: 'Status',
+    createPlan: 'Create Plan',
+    position: 'Position',
+    degree: 'Degree',
+    department: 'Department',
+    departmentDirector: 'Director of the Department',
 }
 
 export default en;
--- src/utils/lang/kz.ts
+++ src/utils/lang/kz.ts
@@ -2,6 +2,18 @@ const kz = {
     loginTitle: 'Кіру',
     password: 'Құпия сөз',
     forgotPassword: 'Құпия сөзді ұмыттыңыз ба?',
+    academicYear: 'Оқу жылы',
+    academicWork: 'Академиялық жұмыс',
+    academicMethods: 'Ак. және әдіс. жұмыс',
+    academicResearchWork: 'Зерттеу жұмысы',
+    academicEducationalWork: 'Тәрбие жұмысы',
+    academicSocialWork: 'Әлеуметтік жұмыс',
+    status: 'Мәртебесі',
+    createPlan: 'Жоспар құру',
+    position: 'Лауазым',
+    degree: 'Ғылыми дәреже',
+    department: 'Департамент',
+    departmentDirector: 'Департамент директоры',
 }
 
 export default kz;
