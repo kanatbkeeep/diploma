3e419a28b058dc26b344c7d050186406d00a1563
kanat.berkinbayev <Kanat.Berkinbayev@nitec.kz>
2023-05-09 20:02:28 +0600

    profile

--- src/components/EditProfile/EditProfile.tsx
+++ src/components/EditProfile/EditProfile.tsx
@@ -3,7 +3,13 @@ import React from "react";
 const EditProfile = (props: any) => {
     return (
         <>
-
+            <div className="editProfileBox">
+                <section></section>
+                <section></section>
+                <section></section>
+                <section></section>
+                <section></section>
+            </div>
         </>
     )
 }
