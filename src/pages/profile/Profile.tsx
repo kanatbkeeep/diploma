3cc795ae82a70f6bc75ac1548e3f1a508319b187
kanat.berkinbayev <Kanat.Berkinbayev@nitec.kz>
2023-05-12 22:55:21 +0600

    profile

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,32 +1,19 @@
 import {observer} from 'mobx-react';
 import React, {useCallback, useEffect, useState} from 'react';
-import Logo from '../../assets/icon/logoSmall.svg'
 import '../../style/profilePage.scss';
-import iconHouse from '../../assets/icon/house.svg'
-import iconBell from '../../assets/icon/bell.svg'
-import iconLogout from '../../assets/icon/logout.svg'
 import Button from "../../components/Button/Button";
-import Table from "../../components/Table/Table";
 import Navigation from "../../components/Navigation/Navigation";
 import AppStore from "../../store/AppStore";
 import t, {l} from "../../utils/Lang";
-import Edit from "../../assets/icon/edit.svg";
-import EditWhite from "../../assets/icon/editWhtie.svg";
-import Copy from "../../assets/icon/copy.svg";
 import {useNavigate} from "react-router-dom";
-import Dropdown from "../../components/Dropdown/Dropdown";
 import EditProfile from "../../components/EditProfile/EditProfile";
 import EditProfileStore from "../../store/EditProfileStore";
 import Plus from '../../assets/icon/plus.svg'
-import TickWhite from "../../assets/icon/tickWhite.svg";
-import CrossWhite from "../../assets/icon/crossWhite.svg";
-import EyeBlack from "../../assets/icon/eyeBlack.svg";
-import DownloadWhite from "../../assets/icon/downloadWhite.svg";
 import Approve from "../../components/Approve/Approve";
-import ApproveStore from "../../store/ApproveStore";
 import DirectorPlanList from "../../components/DirectorPlanList/DirectorPlanList";
 import Notification from "../../components/Notification/Notification";
 import TeacherPlanList from "../../components/TeacherPlanList/TeacherPlanList";
+import UserData from "../../components/UserData/UserData";
 
 function Profile() {
     const [modalOpen, setModalOpen] = useState<boolean>(false);
@@ -43,6 +30,19 @@ function Profile() {
         setApproveOpen(state)
     }, []);
 
+    const setDataForUpdate = () => {
+        EditProfileStore.editModel({
+            firstName: AppStore.currentUser.firstName,
+            lastName: AppStore.currentUser.lastName,
+            middleName: AppStore.currentUser.middleName,
+            position: AppStore.currentUser.position,
+            degree: AppStore.currentUser.degree,
+            rate: AppStore.currentUser.rate,
+            fileBase64: AppStore.currentUser.photo,
+            department: AppStore.department,
+        })
+    }
+
     useEffect(() => {
         AppStore.getUser().then(() => {
             if (!AppStore.currentUser) {
@@ -50,41 +50,15 @@ function Profile() {
             }
 
             if (AppStore.isTeacher()) {
-                AppStore.getDepartmentByTeacher().then(() => {
-                    EditProfileStore.editModel({
-                        firstName: AppStore.currentUser.firstName,
-                        lastName: AppStore.currentUser.lastName,
-                        middleName: AppStore.currentUser.middleName,
-                        position: AppStore.currentUser.position,
-                        degree: AppStore.currentUser.degree,
-                        rate: AppStore.currentUser.rate,
-                        fileBase64: AppStore.currentUser.photo,
-                        department: AppStore.department,
-                    })
-                });
-                AppStore.getMyPlans();
-                EditProfileStore.getPositions();
-                EditProfileStore.getDegrees();
-                EditProfileStore.getDepartmentList();
+                AppStore.getDepartmentByTeacher().then(() => {setDataForUpdate();});
             } else {
-                AppStore.getDepartmentByDirector().then(() => {
-                    EditProfileStore.editModel({
-                        firstName: AppStore.currentUser.firstName,
-                        lastName: AppStore.currentUser.lastName,
-                        middleName: AppStore.currentUser.middleName,
-                        position: AppStore.currentUser.position,
-                        degree: AppStore.currentUser.degree,
-                        rate: AppStore.currentUser.rate,
-                        fileBase64: AppStore.currentUser.photo,
-                        department: AppStore.department,
-                    })
-                });
-                AppStore.getMyPlans();
+                AppStore.getDepartmentByDirector().then(() => {setDataForUpdate();});
                 AppStore.getMyPlansToApproveAwaiting();
-                EditProfileStore.getPositions();
-                EditProfileStore.getDegrees();
-                EditProfileStore.getDepartmentList();
             }
+            AppStore.getMyPlans();
+            EditProfileStore.getPositions();
+            EditProfileStore.getDegrees();
+            EditProfileStore.getDepartmentList();
 
         }).catch(() => {
             if (!AppStore.currentUser) {
@@ -98,52 +72,7 @@ function Profile() {
             <EditProfile store={EditProfileStore} open={modalOpen2} handleChange={handleModalStateChanged2}/>
             <main className={modalOpen || modalOpen2 || approveOpen ? 'darker' : ''}>
                 <Navigation onModalStateChanged={handleModalStateChanged}/>
-
-                <section className={'userInfo mt-38'}>
-                    <aside className={'userAvatar'}>
-                        <img src={AppStore.currentUser?.photo}/>
-                    </aside>
-                    <aside className={'userData'}>
-                        <h2>{AppStore.currentUser?.lastName + ' ' + AppStore.currentUser?.firstName + ' ' + AppStore.currentUser?.middleName}</h2>
-                        <div className="row">
-                            <div className="column mr-58 space-between">
-                                <div>
-                                    <h4 className="mt-24">{t('position')}</h4>
-                                    <span>{AppStore.currentUser?.position[l('name')]}</span>
-                                </div>
-                                {AppStore.isTeacher() ? <div>
-                                    <div>
-                                        <h4 className="mt-24">{t('degree')}</h4>
-                                        <span>{AppStore.currentUser?.degree[l('name')]}</span>
-                                    </div>
-                                </div> : null}
-                                <div>
-                                    <h4 className={AppStore.isTeacher() ? "mt-24" : "mt-96"}>{t('department')}</h4>
-                                    <span>{AppStore.department?.name}</span>
-                                </div>
-                            </div>
-                            <div className="column mr-58 space-between">
-                                {AppStore.isTeacher() ? <div>
-                                    <h4 className="mt-24">{t('rate')}</h4>
-                                    <span>{AppStore.currentUser?.rate}</span>
-                                </div> : null}
-                                {AppStore.isTeacher() ? <div>
-                                    <h4 className="mt-24">{t('departmentDirector')}</h4>
-                                    <span>{AppStore.department?.director.firstName + ' ' + AppStore.department?.director.lastName}</span>
-                                </div> : null}
-                            </div>
-                            <div className="column profileEditBtnParent">
-                                <Button
-                                    className={'profileEditBtn'}
-                                    icon={EditWhite}
-                                    onClick={() => {
-                                        setModalOpen2(true);
-                                    }}
-                                />
-                            </div>
-                        </div>
-                    </aside>
-                </section>
+                <UserData onModalStateChanged={handleModalStateChanged2}/>
 
                 {AppStore.isTeacher() ? <>
                     <TeacherPlanList/>
