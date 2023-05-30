58cb08612eee06236035342b5be9c439bc512a06
ko1z-se-2015 <201402@astanait.edu.kz>
2023-05-30 19:32:19 +0600

    changes

--- src/config/rest/common.ts
+++ src/config/rest/common.ts
@@ -1,4 +1,4 @@
 export const local = `http://localhost:8080`;
 export const production = `https://aitu-plan.herokuapp.com`
-export const isTest = true;
+export const isTest = false;
 export const url = isTest ? local : production;
