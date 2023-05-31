85281bfe81e2ed1dc0b79fc7022a0ba929e94002
kanatbkeeep <clashofkanat@gmail.com>
2023-05-31 12:15:59 +0600

    reportPLan

--- src/components/DirectorPlanList/DirectorPlanList.tsx
+++ src/components/DirectorPlanList/DirectorPlanList.tsx
@@ -11,12 +11,24 @@ import EyeBlack from "../../assets/icon/eyeBlack.svg";
 import DownloadWhite from "../../assets/icon/downloadWhite.svg";
 import {useNavigate} from "react-router-dom";
 import {url} from "../../config/rest/common";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 
 const DirectorPlanList = (props: any) => {
     const navigate = useNavigate();
     return (
         <section className="tableProfile">
             {AppStore.myPlansToApprove ? <>
+                <div className="togglePlansReportDirector">
+                    <ToggleSwitch
+                        text2={t('report')}
+                        text1={t('myPlans')}
+                        checked={AppStore.model.showReport}
+                        onChange={(e: any) => {
+                            AppStore.editModel({showReport: e.target.checked});
+                        }}
+                    />
+                </div>
+
                 <Table
                     array={AppStore.myPlansToApprove}
                     rowsPerPage={4}
@@ -36,57 +48,59 @@ const DirectorPlanList = (props: any) => {
                         </div>
                     }}
                     renderBody={(item, index, maxWidthColumns) => {
-                        return (
-                            <div key={index}>
-                                <div style={{maxWidth: 25}}></div>
-                                <div style={{maxWidth: maxWidthColumns[0]}}>{item.report ? t('report') : t('plan')}</div>
-                                <div style={{maxWidth: maxWidthColumns[1]}}>{item.createdBy.firstName + ' ' + item.createdBy.lastName + ' ' + item.createdBy.middleName}</div>
-                                <div style={{maxWidth: maxWidthColumns[2]}}>{item.year}</div>
-                                <div style={{maxWidth: maxWidthColumns[3]}}>{item.status}</div>
-                                <div style={{maxWidth: maxWidthColumns[4], display: "flex", justifyContent: "space-between"}}>
-                                    <div>
-                                        <Button
-                                            icon={EyeBlack}
-                                            type={'smallWhite'}
-                                            onClick={() => {
-                                                navigate(`/plan/${item.id}`);
-                                            }}
-                                        />
-                                    </div>
-                                    <div>
-                                        <Button
-                                            icon={DownloadWhite}
-                                            type={'smallDark'}
-                                            onClick={()=>{
-                                                window.location.href = `${url}/plan/create-docx?planId=${item.id}`
-                                            }}
-                                        />
-                                    </div>
-                                    <div>
-                                        <Button
-                                            icon={TickWhite}
-                                            type={'smallBlue'}
-                                            disabled={item.status === 'APPROVED'}
-                                            onClick={() => {
-                                                props.onModalStateChangedApprove(true);
-                                                ApproveStore.editModel({selectedPlan: item});
-                                            }}
-                                        />
-                                    </div>
-                                    <div>
-                                        <Button
-                                            icon={CrossWhite}
-                                            type={'smallRed'}
-                                            disabled={item.status === 'APPROVED'}
-                                            onClick={() => {
-                                                props.onModalStateChangedRevision(true);
-                                                ApproveStore.editModel({selectedPlan: item});
-                                            }}
-                                        />
+                        if (item.report === AppStore.model.showReport) {
+                            return (
+                                <div key={index}>
+                                    <div style={{maxWidth: 25}}></div>
+                                    <div style={{maxWidth: maxWidthColumns[0]}}>{item.report ? t('report') : t('plan')}</div>
+                                    <div style={{maxWidth: maxWidthColumns[1]}}>{item.createdBy.firstName + ' ' + item.createdBy.lastName + ' ' + item.createdBy.middleName}</div>
+                                    <div style={{maxWidth: maxWidthColumns[2]}}>{item.year}</div>
+                                    <div style={{maxWidth: maxWidthColumns[3]}}>{item.status}</div>
+                                    <div style={{maxWidth: maxWidthColumns[4], display: "flex", justifyContent: "space-between"}}>
+                                        <div>
+                                            <Button
+                                                icon={EyeBlack}
+                                                type={'smallWhite'}
+                                                onClick={() => {
+                                                    navigate(`/plan/${item.id}`);
+                                                }}
+                                            />
+                                        </div>
+                                        <div>
+                                            <Button
+                                                icon={DownloadWhite}
+                                                type={'smallDark'}
+                                                onClick={()=>{
+                                                    window.location.href = `${url}/plan/create-docx?planId=${item.id}`
+                                                }}
+                                            />
+                                        </div>
+                                        <div>
+                                            <Button
+                                                icon={TickWhite}
+                                                type={'smallBlue'}
+                                                disabled={item.status === 'APPROVED'}
+                                                onClick={() => {
+                                                    props.onModalStateChangedApprove(true);
+                                                    ApproveStore.editModel({selectedPlan: item});
+                                                }}
+                                            />
+                                        </div>
+                                        <div>
+                                            <Button
+                                                icon={CrossWhite}
+                                                type={'smallRed'}
+                                                disabled={item.status === 'APPROVED'}
+                                                onClick={() => {
+                                                    props.onModalStateChangedRevision(true);
+                                                    ApproveStore.editModel({selectedPlan: item});
+                                                }}
+                                            />
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        );
+                            );
+                        } else return <></>
                     }}
                     search={true}/>
             </> : null}
