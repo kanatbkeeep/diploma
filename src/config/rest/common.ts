8bbebb05051f25b7edcb91e59c7b66f05d758f9d
kanatbkeeep <clashofkanat@gmail.com>
2023-05-31 16:52:45 +0600

    deploy

--- src/config/rest/common.ts
+++ src/config/rest/common.ts
@@ -1,4 +1,4 @@
 export const local = `http://localhost:8080`;
-export const production = `https://plan-back-deploy.vercel.app`
+export const production = `https://plan-back-deploy-2wjz8653g-kanatbkeeep.vercel.app/`
 export const isTest = false;
 export const url = isTest ? local : production;
