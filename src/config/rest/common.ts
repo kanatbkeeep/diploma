38d89b4bb01d4f8a5ff85465c8924a0d1ec11b9d
kanatbkeeep <clashofkanat@gmail.com>
2023-05-31 15:41:41 +0600

    deploy

--- src/config/rest/common.ts
+++ src/config/rest/common.ts
@@ -1,4 +1,4 @@
 export const local = `http://localhost:8080`;
-export const production = `https://ipp-back.herokuapp.com`
+export const production = `https://ipp-aitu.herokuapp.com`
 export const isTest = false;
 export const url = isTest ? local : production;
